4e119940e55618c50707f5d9925b6d889f2ff1ff
Nitesh06 <niteshkumawat6070@gmail.com>
2026-01-05 19:32:18 +0530

    first commit

---
+++ server/configs/db.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected',()=>console.log('MongoDB connected successfully'));
+        await mongoose.connect(process.env.MONGO_URI as string);
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+}
+export default connectDB;
